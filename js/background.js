065dac3cf5687b75511fdf97844e8c68863594d8
choimarmot <148835532+choimarmot@users.noreply.github.com>
2024-01-28 20:21:13 +0900

    second commit

--- js/background.js
+++ js/background.js
@@ -2,8 +2,4 @@ const images = ["0.jpg", "1.jpg", "2.jpg"]; // 사진과 이름 같아야 함
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-bgImage.className = 'bg';
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `url(img/${chosenImage}`;
